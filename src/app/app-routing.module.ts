be117b232fb396ab57f1c5a78164130b6e459144
Anesto27 <anes.2503@gmx.at>
2019-02-22 23:09:44 +0100

    finish style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ImportantComponent } from './important/important.component';
+import { BookPageComponent } from './book-page/book-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+        path:"",component: HomePageComponent
+},
+{
+        path:"Important numbers",component: ImportantComponent
+}
+{
+        path:"My phone book",component: BookPageComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ImportantComponent } from './important/important.component';
+import { BookPageComponent } from './book-page/book-page.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    HomePageComponent,
+    ImportantComponent,
+    BookPageComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
